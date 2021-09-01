e184ce7e674caa222a2bbe24d85c840722e6ae8a
mayone-du <cocomayo1201@icloud.com>
2021-09-01 23:15:32 +0900

    Fix some

--- src/pages/_/Layouts/Footer.tsx
+++
@@ -1,3 +0,0 @@
-export const Footer: React.VFC = () => {
-  return <footer className="py-4 text-center text-white bg-black">copyright</footer>;
-};
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,7 @@
 // import type { NextPage } from "next";
 import { useSession } from "next-auth/client";
 import { ThemeChanger } from "src/components/ThemeChanger";
-import { useCreateUserMutation, useGetUserLazyQuery } from "src/graphql/schemas/schema";
+import { useGetUserLazyQuery } from "src/graphql/schemas/schema";
 import { MainLayout } from "src/pages/_/Layouts/MainLayout";
 
 const IndexPage: any = () => {
@@ -9,14 +9,11 @@ const IndexPage: any = () => {
   const [getUserLazyQuery, { data: userData }] = useGetUserLazyQuery({
     fetchPolicy: "network-only",
   });
-  const [createUserMutation] = useCreateUserMutation();
   // eslint-disable-next-line no-console
   console.log("IndexPage Session", session);
 
   const handleClick = async () => {
     getUserLazyQuery({ variables: { id: "VXNlck5vZGU6Mg==" } });
-    const { data } = await createUserMutation();
-    console.log(data);
   };
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
